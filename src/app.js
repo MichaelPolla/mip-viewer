e6b8862645c1ce21af444e5f239a06df8ad247be
Michaël Polla <michael@polla.net>
2018-05-10 23:21:10 +0200

    Add a 'true' copy of three-gltf-viewer instead of a submodule. I want to make a subtree of this folder, as it's the app root, for heroku. I can't figure out how to make a subtree of a submodule, so maybe this will work.

---
+++ src/app.js
@@ -0,0 +1,153 @@
+const Detector = require('three/examples/js/Detector');
+const Viewer = require('./viewer');
+const SimpleDropzone = require('simple-dropzone');
+const ValidationController = require('./validation-controller');
+const queryString = require('query-string');
+
+if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
+  console.error('The File APIs are not fully supported in this browser.');
+} else if (!Detector.webgl) {
+  console.error('WebGL is not supported in this browser.');
+}
+
+class App {
+
+  /**
+   * @param  {Element} el
+   * @param  {Location} location
+   */
+  constructor (el, location) {
+
+    const hash = location.hash ? queryString.parse(location.hash) : {};
+    this.options = {
+      kiosk: Boolean(hash.kiosk),
+      model: hash.model || '',
+      preset: hash.preset || '',
+      cameraPosition: hash.cameraPosition
+        ? hash.cameraPosition.split(',').map(Number)
+        : null
+    };
+
+    this.el = el;
+    this.viewer = null;
+    this.viewerEl = null;
+    this.spinnerEl = el.querySelector('.spinner');
+    this.dropEl = el.querySelector('.dropzone');
+    this.inputEl = el.querySelector('#file-input');
+    this.validationCtrl = new ValidationController(el);
+
+    this.createDropzone();
+    this.hideSpinner();
+
+    const options = this.options;
+
+    if (options.kiosk) {
+      const headerEl = document.querySelector('header');
+      headerEl.style.display = 'none';
+    }
+
+    if (options.model) {
+      this.view(options.model, '', new Map());
+    }
+  }
+
+  /**
+   * Sets up the drag-and-drop controller.
+   */
+  createDropzone () {
+    const dropCtrl = new SimpleDropzone(this.dropEl, this.inputEl);
+    dropCtrl.on('drop', ({files}) => this.load(files));
+    dropCtrl.on('dropstart', () => this.showSpinner());
+    dropCtrl.on('droperror', () => this.hideSpinner());
+  }
+
+  /**
+   * Sets up the view manager.
+   * @return {Viewer}
+   */
+  createViewer () {
+    this.viewerEl = document.createElement('div');
+    this.viewerEl.classList.add('viewer');
+    this.dropEl.innerHTML = '';
+    this.dropEl.appendChild(this.viewerEl);
+    this.viewer = new Viewer(this.viewerEl, this.options);
+    return this.viewer;
+  }
+
+  /**
+   * Loads a fileset provided by user action.
+   * @param  {Map<string, File>} fileMap
+   */
+  load (fileMap) {
+    let rootFile;
+    let rootPath;
+    Array.from(fileMap).forEach(([path, file]) => {
+      if (file.name.match(/\.(gltf|glb)$/)) {
+        rootFile = file;
+        rootPath = path.replace(file.name, '');
+      }
+    });
+
+    if (!rootFile) {
+      this.onError('No .gltf or .glb asset found.');
+    }
+
+    this.view(rootFile, rootPath, fileMap);
+  }
+
+  /**
+   * Passes a model to the viewer, given file and resources.
+   * @param  {File|string} rootFile
+   * @param  {string} rootPath
+   * @param  {Map<string, File>} fileMap
+   */
+  view (rootFile, rootPath, fileMap) {
+
+    if (this.viewer) this.viewer.clear();
+
+    const viewer = this.viewer || this.createViewer();
+
+    const fileURL = typeof rootFile === 'string'
+      ? rootFile
+      : URL.createObjectURL(rootFile);
+
+    const cleanup = () => {
+      this.hideSpinner();
+      if (typeof rootFile === 'object') URL.revokeObjectURL(fileURL);
+    };
+
+    viewer
+      .load(fileURL, rootPath, fileMap)
+      .catch((e) => this.onError(e))
+      .then(cleanup);
+
+    if (!this.options.kiosk) {
+      this.validationCtrl.validate(fileURL, rootPath, fileMap);
+    }
+  }
+
+  /**
+   * @param  {Error} error
+   */
+  onError (error) {
+    if (error && error.target && error.target instanceof Image) {
+      error = 'Missing texture: ' + error.target.src.split('/').pop();
+    }
+    window.alert((error||{}).message || error);
+    console.error(error);
+  }
+
+  showSpinner () {
+    this.spinnerEl.style.display = '';
+  }
+
+  hideSpinner () {
+    this.spinnerEl.style.display = 'none';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const app = new App(document.body, location);
+
+});
